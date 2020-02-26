71a749394ae9f57cd80ff952044c7a833c976d83
WIW DEV <john@wiwsem.com>
2020-02-25 18:55:14 -0800

    changed output towards csv format and added new analysis fields total words and average word length

--- ripDebate.py
+++ ripDebate.py
@@ -3,7 +3,7 @@ import re, pprint
 
 debug = False
 
-with open('debateWithSpaces.txt', 'r') as rf:
+with open('./debateWithSpaces.txt', 'r') as rf:
   text = rf.read()
 
 #key: [total spoken lines, crosstalk events, total chars]
@@ -30,13 +30,14 @@ for line in x:
       test = d[currentKeyName]
     except:
       print ('Created dict entry for ' + currentKeyName)
-      d[currentKeyName] = [0,0,0]
+      d[currentKeyName] = [0,0,0,'']  # adding a string to cobble it all together for linguistic complexity analysis
  
     try:
       d[currentKeyName][0] = d[currentKeyName][0] + 1
       if line.find('CROSSTALK') >= 0:
         d[currentKeyName][1] = d[currentKeyName][1] + 1
       d[currentKeyName][2] = d[currentKeyName][2] + len(line)
+      d[currentKeyName][3] = d[currentKeyName][3] + ' ' + line
     except:
       pass
     currentKeyName = ''
@@ -47,5 +48,17 @@ for line in x:
 
 
 if debug: print ('\n - - - FINISHED ' + str(lCtr) + ' lines - - - \n')
-print ('#output format is: PERSON: [total spoken lines, crosstalk events, total chars]')
-pprint.pprint(d)
+print ('#output format is: PERSON, total spoken lines, crosstalk events, total chars, total words, average word length')
+if debug: pprint.pprint(d)
+for i in d:
+  wordSalad = d[i][3].split()
+  totalWords = len(wordSalad)
+  sum = 0
+  for word in wordSalad:
+        ch = len(word)
+        sum = sum + ch
+  avg = float(sum) / totalWords
+  # csv compatible formatting
+  print (i + ',' + str(d[i][0]) + ',' + str(d[i][1]) + ',' + str(d[i][2]) + ',' + str(totalWords) + ',' + str(round(avg,2)))
+
+

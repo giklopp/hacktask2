56a49fff2e0fa405f1fc9d6e1a62e6c4699436c9
WIW DEV <john@wiwsem.com>
2020-02-25 18:08:27 -0800

    made dict name keys dynamic

--- ripDebate.py
+++ ripDebate.py
@@ -7,7 +7,8 @@ with open('debateWithSpaces.txt', 'r') as rf:
   text = rf.read()
 
 #key: [total spoken lines, crosstalk events, total chars]
-# TODO add all contestant and moderator names
+# removed names and set them dynamically ar runtime
+'''
 d = {'BIDEN': [0,0,0],
  'BLOOMBERG': [0,0,0],
  'SANDERS': [0,0,0],
@@ -15,13 +16,22 @@ d = {'BIDEN': [0,0,0],
  'KLOBUCHAR': [0,0,0],
  'BUTTIGIEG': [0,0,0],
  'HOLT': [0,0,0]}
+'''
 
+d = {}
 lCtr = 0
 currentKeyName = ''
 
 x = re.split('(\n[A-Z]*:)',text)
 for line in x:
   if currentKeyName != '':
+    # first see if there is a dict entry for this key.  If not, catch the error and create one
+    try:
+      test = d[currentKeyName]
+    except:
+      print ('Created dict entry for ' + currentKeyName)
+      d[currentKeyName] = [0,0,0]
+ 
     try:
       d[currentKeyName][0] = d[currentKeyName][0] + 1
       if line.find('CROSSTALK') >= 0:
@@ -32,13 +42,10 @@ for line in x:
     currentKeyName = ''
   if line.endswith(':'):
     currentKeyName = line[0:len(line)-1].strip()
-    if debug: print 'PROCESS line for ' + currentKeyName + ' ~ ' + x[lCtr + 1]
+    if debug: print ('PROCESS line for ' + currentKeyName + ' ~ ' + x[lCtr + 1])
   lCtr = lCtr + 1
 
 
-if debug: print '\n - - - FINISHED ' + str(lCtr) + ' lines - - - \n'
-print '#output format is: PERSON: [total spoken lines, crosstalk events, total chars]'
-pp = pprint.PrettyPrinter(indent=4)
-print pp.pprint(d)
-# versus an ugly print
-print str(d)
+if debug: print ('\n - - - FINISHED ' + str(lCtr) + ' lines - - - \n')
+print ('#output format is: PERSON: [total spoken lines, crosstalk events, total chars]')
+pprint.pprint(d)

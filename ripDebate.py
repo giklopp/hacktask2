d71930190726506c661eccb568cfabe1c64e5bb7
WIW DEV <john@wiwsem.com>
2020-02-26 18:13:30 -0800

    added csv header and renamed lines to CHUNKS because thats CHUNKY

--- ripDebate.py
+++ ripDebate.py
@@ -14,9 +14,9 @@ d = {}  # hold all the data here using NAME as the key
 lCtr = 0  # a line counter, not used for much but a sanity check
 currentKeyName = '' # holds the current key name as we iterate thru the text data
 
-x = re.split('(\n[A-Z]*:)',text)  # chop up the data with re using NAME:
+chunky = re.split('(\n[A-Z]*:)',text)  # chop up the data with re using NAME:
 
-for line in x:  # look at each chunk of the split up file
+for chunk in chunky:  # look at each chunk of the split up file
 
   if currentKeyName != '':
     # see if there is already a dict entry for this key.  If not, catch the error and create one
@@ -28,24 +28,25 @@ for line in x:  # look at each chunk of the split up file
  
     try:  # populate the data here
       d[currentKeyName][0] = d[currentKeyName][0] + 1
-      if line.find('CROSSTALK') >= 0:
+      if chunk.find('CROSSTALK') >= 0:
         d[currentKeyName][1] = d[currentKeyName][1] + 1
-      d[currentKeyName][2] = d[currentKeyName][2] + len(line)
-      d[currentKeyName][3] = d[currentKeyName][3] + ' ' + line
+      d[currentKeyName][2] = d[currentKeyName][2] + len(chunk)
+      d[currentKeyName][3] = d[currentKeyName][3] + ' ' + chunk 
     except:
       pass
     currentKeyName = ''  # reset currentKeyName for the next chunk
 
-  if line.endswith(':'):
-    currentKeyName = line[0:len(line)-1].strip()
-    if debug: print ('PROCESS line for ' + currentKeyName + ' ~ ' + x[lCtr + 1])
+  if chunk.endswith(':'):
+    currentKeyName = chunk[0:len(chunk)-1].strip()
+    if debug: print ('PROCESS chunk for ' + currentKeyName + ' ~ ' + x[lCtr + 1])
   lCtr = lCtr + 1
 
 
-print ('\n - - - FINISHED ' + str(lCtr) + ' lines - - - \n')
+print ('\n - - - FINISHED ' + str(lCtr) + ' chunks - - - \n')
 print ('#output format is: PERSON, total spoken lines, crosstalk events, total chars, total words, average word length')
 # write this to a file
 output = open("results.csv", "a")
+output.write('name,totalspokenlines,crosstalkevents,totalchars,totalwords,averagewordlength\n')
 if debug: pprint.pprint(d)
 for i in sorted(d):  # for extra credit add some word stats
   wordSalad = d[i][3].split()
